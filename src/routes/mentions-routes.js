27f8e0e454f94667a61496fd174546d2c0a3da80
cdfortes <cdfortes@gmail.com>
2019-07-14 14:25:35 -0100

    refatoration, validation of mentions

--- src/routes/mentions-routes.js
+++ src/routes/mentions-routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 const mentionsController = require('../controllers/mentions-controller');
 
 router.get('/', mentionsController.listMentions);
-router.post('/', mentionsController.createMentions);
+router.post('/', [
+    check('friend').isLength({ min: 7 }).withMessage("The name need 7 character min"),
+    check('mention').isLength({ min: 20, max: 280 }).withMessage("The mention need 20 character min and 280 max.")
+], mentionsController.createMentions);
 
 module.exports = router;
